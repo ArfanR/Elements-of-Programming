072d0f5e8ff89af960ee5e527822cf147470e119
Arfan Rehab <areha308@gmail.com>
2017-07-19 17:03:51 -0700

    Dutch flag

--- arrays/src/main/DutchNationalFlag.java
+++ arrays/src/main/DutchNationalFlag.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import java.util.*;
+import java.lang.*;
 
 public class DutchNationalFlag {
 
@@ -7,6 +8,26 @@ public class DutchNationalFlag {
     */
 
     public static void dutchNationalFlag(int p, List<Integer> A) {
+        if (p < 0 || p >= A.size()) {
+            System.out.println("Invalid index");
+        }
 
+        int pivot = A.get(p);
+        int smaller = 0;
+        int equal = 0;
+        int larger = A.size()-1;
+
+        while (equal <= larger) {
+            if (A.get(equal) < pivot) {
+                Collections.swap(A, A.get(equal++), A.get(smaller++));
+            }
+            else if (A.get(equal) == pivot) {
+                equal++;
+            }
+            else if (A.get(equal > pivot) {
+                Collections.swap(A, A.get(equal), A.get(larger--));
+            }
+        }
     }
+
 }
