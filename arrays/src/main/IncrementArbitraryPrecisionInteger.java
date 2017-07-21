f5b47d5ad14615e2ab0a107af23d6a5b5caa4208
Arfan Rehab <areha308@gmail.com>
2017-07-21 00:14:15 -0700

    New version

--- arrays/src/main/IncrementArbitraryPrecisionInteger.java
+++ arrays/src/main/IncrementArbitraryPrecisionInteger.java
@@ -7,6 +7,35 @@ public class IncrementArbitraryPrecisionInteger {
     */
 
     public static List<Integer> incrementInteger(List<Integer> A) {
+        int n = A.size()-1;
+        A.set(n, A.get(n)+1);
+        for (int i = n; i > 0 && A.get(i) == 10; i--) {
+            A.set(i, 0);
+            A.set(i-1, A.get(i-1) + 1);
+        }
+        if (A.get(0) == 10) {
+            A.set(0, 0);
+            A.add(0, 1);
+        }
         return A;
+    }
+
+    public static List<Integer> incrementInteger2(List<Integer> A) {
+        int n = A.size()-1;
+        int carry = 0;
+        for (int i = n; i >= 0; i--) {
+            if (carry + A.get(i) >= 10) {
+                A.set(i, 0);
+                carry = 1;
+            }
+            else {
+                A.set(i, carry + A.get(i));
+                break;
+            }
+        }
+        if (A.get(0) == 0 && carry == 1) {
+            A.set(0, 0);
+            A.add(0, 1);
+        }
     }
 }
