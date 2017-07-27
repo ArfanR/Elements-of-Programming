c257d151b96ea5792b0d3f2391768f42ec560d7c
Arfan Rehab <areha308@gmail.com>
2017-07-26 23:49:31 -0700

    Finish multiplication

--- arrays/src/main/MultipleArbitraryPrecisionIntegers.java
+++ arrays/src/main/MultipleArbitraryPrecisionIntegers.java
@@ -8,7 +8,30 @@ public class MultipleArbitraryPrecisionIntegers {
     */
 
     public static List<Integer> multiply(List<Integer> a, List<Integer> b) {
+        final int sign = a.get(0) < 0 ^ b.get(0) < 0 ? -1 : 1;
+        a.set(0, Math.abs(a.get(0)));
+        b.set(0, Math.abs(b.get(0)));
 
-        return Collections.emptyList();
+        List<Integer> result = new List<Integer>(Collections.nCopies(a.size() + b.size(), 0));
+        for (int i = a.size()-1; i >= 0; i--) {
+            for (int j = b.size()-1; j >= 0; j--) {
+                result.set(i+j+1, result.get(i+j+1) + a.get(i) * b.get(j));
+                result.set(i+j, result.get(i+j) + result.get(i+j+1)/10);
+                result.set(i+j+1, result.get(i+j+1) % 10);
+            }
+        }
+
+        // remove leading zeroes
+        int firstNotZero = 0;
+        while (firstNotZero < result.size() && result.get(firstNotZero) == 0) {
+            firstNotZero++;
+        }
+        result = result.subList(firstNotZero, result.size());
+
+        if (result.isEmpty()) {
+            return Arrays.asList(0);
+        }
+        result.set(0, result.get(0) * sign);
+        return result;
     }
 }
