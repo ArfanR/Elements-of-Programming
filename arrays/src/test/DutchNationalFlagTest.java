d3de70f822cc730e1394dac73452f1f1cea2f1c8
Arfan Rehab <areha308@gmail.com>
2017-07-27 23:53:00 -0700

    Fix test cases

--- arrays/src/test/DutchNationalFlagTest.java
+++ arrays/src/test/DutchNationalFlagTest.java
@@ -25,7 +25,7 @@ public class DutchNationalFlagTest {
     public void dutchNationalFlag2() {
         pivot = 4;
         unordered = Arrays.asList(3, 3, 3, 2, 2, 2, 1, 1, 1);
-        ordered = Arrays.asList(2, 2, 3, 3, 3, 2, 1, 1, 1);
+        ordered = Arrays.asList(1, 1, 1, 2, 2, 2, 3, 3, 3);
 
         test(pivot, unordered, ordered);
     }
@@ -35,24 +35,6 @@ public class DutchNationalFlagTest {
         pivot = 8;
         unordered = Arrays.asList(3, 3, 3, 2, 2, 2, 1, 1, 1);
         ordered = Arrays.asList(1, 1, 1, 2, 2, 2, 3, 3, 3);
-
-        test(pivot, unordered, ordered);
-    }
-
-    @Test
-    public void dutchNationalFlag4() {
-        pivot = 7;
-        unordered = Arrays.asList(5, 4, 2, 3, 5, 1, 7, 6, 4, 9, 6, 2, 4, 3, 2, 4, 6, 7, 8);
-        ordered = Arrays.asList(5, 4, 2, 3, 5, 1, 6, 7, 4, 9, 6, 2, 4, 3, 2, 4, 6, 7, 8);
-
-        test(pivot, unordered, ordered);
-    }
-
-    @Test
-    public void dutchNationalFlag5() {
-        pivot = 18;
-        unordered = Arrays.asList(5, 4, 2, 3, 5, 1, 7, 6, 4, 9, 6, 2, 4, 3, 2, 4, 6, 7, 8);
-        ordered = Arrays.asList(5, 4, 2, 3, 5, 1, 7, 6, 4, 6, 2, 4, 3, 2, 4, 6, 7, 8, 9);
 
         test(pivot, unordered, ordered);
     }
