cf0a2a7002e6b8b7833956bb08b830888fe4c178
Arfan Rehab <areha308@gmail.com>
2017-08-03 03:28:27 -0700

    Max profit

--- arrays/src/main/BuySellStockOnce.java
+++ arrays/src/main/BuySellStockOnce.java
@@ -6,7 +6,12 @@ public class BuySellStockOnce {
        5.6
     */
 
-    public static int buySellStockOnce(List<Integer> A) {
-        return 0;
+    public static int buySellStockOnce(List<Integer> prices) {
+    	int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (Integer price : prices) {
+          maxProfit = Math.max(maxProfit, price - minPrice);
+          minPrice = Math.min(minPrice, price);
+        }
+        return maxProfit;
     }
 }
--- arrays/src/test/BuySellStockOnceTest.java
+++ arrays/src/test/BuySellStockOnceTest.java
@@ -12,22 +12,34 @@ public class BuySellStockOnceTest {
 
     @Test
     public void buySellStockOnce1() {
-       
+        prices = Arrays.asList(310, 315, 275, 295, 260, 270, 290, 230, 255, 250);
+        maxProfit = 30;
+
+        test(prices, maxProfit);
     }
 
     @Test
     public void buySellStockOnce2() {
-    
+        prices = Arrays.asList(100, 200);
+        maxProfit = 100;
+
+        test(prices, maxProfit);
     }
 
     @Test
     public void buySellStockOnce3() {
-     
+        prices = Arrays.asList(200, 100);
+        maxProfit = 0;
+
+        test(prices, maxProfit);
     }
 
     @Test
     public void buySellStockOnce4() {
-       
+        prices = Arrays.asList(2, 4, 2, 6, 3, 0, 5);
+        maxProfit = 5;
+
+        test(prices, maxProfit);
     }
 
     private void test(List<Integer> prices, int maxProfit) {
