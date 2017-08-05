efcf3108bb0c0728d98cff8a069c1bdba057ba7a
Arfan Rehab <areha308@gmail.com>
2017-08-05 00:11:42 -0700

    Max profit stock twice

--- arrays/src/main/BuySellStockTwice.java
+++ arrays/src/main/BuySellStockTwice.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class BuySellStockTwice {
 
@@ -6,9 +6,25 @@ public class BuySellStockTwice {
     5.7
     */
 
-    public static int buySellStockTwice(List<Integer> A) {
-
-        return 0;
+    public static int buySellStockTwice(List<Integer> prices) {
+    	int maxTotalProfit = 0;
+        List<Integer> firstBuySellProfits = new ArrayList<>();
+        int minPriceSoFar = Integer.MAX_VALUE;
+        // Forward phase. For each day, we record maximum profit if we
+        // sell on that day.
+        for (int i = 0; i < prices.size(); ++i) {
+        	minPriceSoFar = Math.min(minPriceSoFar, prices.get(i));
+        	maxTotalProfit = Math.max(maxTotalProfit, prices.get(i) - minPriceSoFar);
+        	firstBuySellProfits.add(maxTotalProfit);
+        }
+        // Backward phase. For each day, find the maximum profit if we make
+        // the second buy on that day.
+        int maxPriceSoFar = Integer.MIN_VALUE;
+        for (int i = prices.size() - 1; i > 0; --i) {
+        	maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+        	maxTotalProfit = Math.max(maxTotalProfit, maxPriceSoFar - prices.get(i) + firstBuySellProfits.get(i - 1));
+        }
+        return maxTotalProfit;
     }
 
 }
