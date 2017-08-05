118218107f7cb87d82eb149677b9d9c7fcc8af1b
Arfan Rehab <areha308@gmail.com>
2017-08-05 06:56:28 -0700

    String to integer

--- strings/src/main/InterconvertStringAndInteger.java
+++ strings/src/main/InterconvertStringAndInteger.java
@@ -5,8 +5,21 @@ public class InterconvertStringAndInteger {
     */
 
     public static Integer stringToInt(String number) {
-
-        return 0;
+    	Integer num = new Integer(0);
+    	int i = 0; 
+    	boolean isNeg = false;
+    	if (number.charAt(0) == '-') {
+    		isNeg = true;
+    		i = 1;
+    	}
+    	while (i < len) {
+    		num *= 10;
+    		num += (number.charAt(i++) - '0');
+    	}
+    	if (isNeg) {
+    		num = -1*num;
+    	}
+    	return num;
     }
 
     public static String intToString(Integer number) {
