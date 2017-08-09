9d5bb140407e296cd44628685ce45989d189b16f
Arfan Rehab <areha308@gmail.com>
2017-08-09 01:09:02 -0700

    Enumerate primes

---
+++ arrays/src/main/EnumeratePrimes.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class EnumeratePrimes {
+
+    /*
+    5.8
+    */
+
+    public static List<Integer> generatePrimes(int n) {
+    	List<Integer> primes = new ArrayList<>();
+    	// isPrime.get(p) represents if p is prime or not. Initially, set each
+        // to true, excepting 0 and 1. Then use sieving to eliminate nonprimes.
+    	List<Boolean> isPrime = new ArrayList<>(Collections.nCopies(n + 1, true));
+        isPrime.set(0, false);
+        isPrime.set(1, false);
+        for (int p = 2; p <= n; ++p) {
+        	if (isPrime.get(p)) {
+        		primes.add(p);
+        		// Sieve p's multiples.
+        		for (int i = p; i <= n; i += p) {
+        			isPrime.set(i, false);
+        		}
+        	}
+        }
+        return primes;
+      }
+}
---
+++ arrays/src/test/EnumeratePrimesTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnumeratePrimesTest {
+
+    private List<Integer> primes;
+    private int n;
+    
+}
