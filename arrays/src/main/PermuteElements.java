7ff80a48fe87e9515a945e6391a8da317ae5f855
Arfan Rehab <areha308@gmail.com>
2017-08-12 05:55:55 -0700

    Permute elements

--- arrays/src/main/PermuteElements.java
+++ arrays/src/main/PermuteElements.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class PermuteElements {
 
@@ -6,7 +6,24 @@ public class PermuteElements {
     5.9
     */
 
-    public static void applyPermutation(List<Integer> perm, List<Integer> a) {
-
-    }
+	public static void applyPermutation(List<Integer> perm, List<Integer> A) {
+	    for (int i = 0; i < A.size(); ++i) {
+	    	// Check if the element at index i has not been moved by checking if
+	    	// perm.get(i) is nonnegative.
+	    	int next = i;
+	    	while (perm.get(next) >= 0) {
+	    		Collections.swap(A, i, perm.get(next));
+	    		int temp = perm.get(next);
+	    		// Subtracts perm.size() from an entry in perm to make it negative,
+	    		// which indicates the corresponding move has been performed.
+	    		perm.set(next, perm.get(next) - perm.size());
+	    		next = temp;
+	    	}
+	    }
+	    // Restore perm.
+	    for (int i = 0; i < perm.size(); i++) {
+	    	perm.set(i, perm.get(i) + perm.size());
+	    }
+	}
+	
 }
