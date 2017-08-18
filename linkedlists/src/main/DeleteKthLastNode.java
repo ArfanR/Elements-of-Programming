49da30ee505105172db29e660f4aa05a4323c7c1
Arfan Rehab <areha308@gmail.com>
2017-08-18 03:13:52 -0700

    Delete kth last node

---
+++ linkedlists/src/main/DeleteKthLastNode.java
@@ -0,0 +1,21 @@
+public class DeleteKthLastNode {
+ 
+	// Assumes L has at least k nodes, deletes the k-th last node in L.
+	public static ListNode<Integer> deleteNode(ListNode<Integer> L, int k) {
+		ListNode<Integer> dummyHead = new ListNode<>(0, L);
+		ListNode<Integer> first = dummyHead.next;
+		while (k-- > 0) {
+			first = first.next;
+		}	
+
+		ListNode<Integer> second = dummyHead;
+		while (first != null) {
+			second = second.next;
+			first = first.next;
+		}
+		// second points to the (k + 1)-th last node, deletes its successor.
+		second.next = second.next.next;
+		return dummyHead.next;
+	}
+  
+}
