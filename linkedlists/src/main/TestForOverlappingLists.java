71efa3eda075e41d287083790e8fced31c9bae8a
Arfan Rehab <areha308@gmail.com>
2017-08-19 13:38:07 -0700

    Delete node test

---
+++ linkedlists/src/main/TestForOverlappingLists.java
@@ -0,0 +1,11 @@
+public class TestForOverlappingLists {
+
+    /*
+    7.4
+    */
+
+    public static ListNode<Integer> doListsOverlap(ListNode<Integer> list1, ListNode<Integer> list2) {
+
+        return new ListNode<>(null);
+    }
+}
