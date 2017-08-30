3d5af09c742a5ea18a606573b4f8fa0eb3592131
Arfan Rehab <areha308@gmail.com>
2017-08-29 23:32:16 -0700

    Stack with max

--- stacksandqueues/src/main/StackWithMax.java
+++ stacksandqueues/src/main/StackWithMax.java
@@ -1,22 +1,62 @@
-public class StackWithMax {
+import java.util.*;
 
+public class StackWithMax {
     /*
     8.1
     */
+	private class MaxWithCount {
+	    public Integer max;
+	    public Integer count;
+	    public MaxWithCount(Integer max, Integer count) {
+	    	this.max = max;
+	    	this.count = count;
+	    }
+	}
+	
+	private Deque<Integer> element = new LinkedList<>();
+    private Deque<MaxWithCount> cachedMaxWithCount = new LinkedList<>();
 
-    public StackWithMax() {
-
-    }
-
+    public boolean empty() { return element.isEmpty(); }
+    
     public Integer max() {
-        return 0;
+    	if (empty()) {
+    		throw new IllegalStateException("max(): empty stack");
+    	}
+        return cachedMaxWithCount.peekFirst().max;
     }
 
     public Integer pop() {
-        return 0;
+    	if (empty()) {
+    		throw new IllegalStateException("pop(): empty stack");
+    	}
+    	Integer popElement = element.removeFirst();
+    	if (popElement.equals(cachedMaxWithCount.peekFirst().max)) {
+    		cachedMaxWithCount.peekFirst().count -= 1;
+    		if (cachedMaxWithCount.peekFirst().count.equals(0)) {
+    			cachedMaxWithCount.removeFirst();
+    		}
+    	}
+    	return popElement;
     }
 
     public void push(Integer x) {
-
+    	element.addFirst(x);
+    	if (!cachedMaxWithCount.isEmpty()) {
+    		if (Integer.compare(x, cachedMaxWithCount.peekFirst().max) == 0) {
+    			cachedMaxWithCount.peekFirst().count += 1;
+    		} 
+    		else if (Integer.compare(x, cachedMaxWithCount.peekFirst().max) > 0) {
+    			cachedMaxWithCount.addFirst(new MaxWithCount(x, 1));
+    		}
+    	} 
+    	else {
+    		cachedMaxWithCount.addFirst(new MaxWithCount(x, 1));
+    	}
     }
+    
 }
+
+
+ 
+
+ 
