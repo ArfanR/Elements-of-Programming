595b04407bfef2b130c16721caea34ec9d6cd4a1
Arfan Rehab <areha308@gmail.com>
2017-09-22 13:51:31 -0700

    First greater than

---
+++ binarysearchtrees/src/main/FirstGreaterThan.java
@@ -0,0 +1,20 @@
+public class FirstGreaterThan {
+
+    /*
+    14.2
+    */
+
+    public static BinaryTree<Integer> find(BinaryTree<Integer> tree, Integer k) {
+    	BinaryTree<Integer> subtree = tree, firstSoFar = null;
+    	while (subtree != null) {
+    		if (subtree.data > k) {
+    			firstSoFar = subtree;
+    			subtree = subtree.left;
+    		}
+    		else {
+    			subtree = subtree.right;
+    		}
+    	}
+    	return firstSoFar;
+    }
+}
