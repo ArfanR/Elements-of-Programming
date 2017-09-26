938c9ac2e325d24c8efb44e67f9efa8b05049c24
Arfan Rehab <areha308@gmail.com>
2017-09-26 07:31:38 -0700

    Compute LCA BST

--- binarysearchtrees/src/main/ComputeLCA.java
+++ binarysearchtrees/src/main/ComputeLCA.java
@@ -5,6 +5,15 @@ public class ComputeLCA {
     */
 
     public static BinaryTree<Integer> findLCA(BinaryTree<Integer> tree, BinaryTree<Integer> s, BinaryTree<Integer> b) {
- 
+    	BinaryTree<Integer> p = tree;
+    	while (p.data < s.data || p.data > b.data) {
+    		while (p.data < s.data) {
+    			p = p.right;
+    		}
+    		while (p.data > b.data) {
+    			p = p.left;
+    		}
+    	}
+    	return p;
     }
 }
