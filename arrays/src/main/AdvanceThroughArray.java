65292d407170a474bf458b238d820f09cb16dff3
Arfan Rehab <areha308@gmail.com>
2017-10-20 04:36:47 -0700

    Remove old solution

--- arrays/src/main/AdvanceThroughArray.java
+++ arrays/src/main/AdvanceThroughArray.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class AdvanceThroughArray {
 
-    /*
-       5.4
+    /* 
+    5.4
     */
 	
 	public static boolean arrayAdvance(List<Integer> A) {
@@ -25,36 +25,5 @@ public class AdvanceThroughArray {
 		}
 		return minSteps;
 	}
-	
-	// Solution will not work for large entries with large possibilities 
-    public static boolean arrayAdvance3(List<Integer> A) {
-    	int prevPos = 0, currPos = 0;
-    	int prevVal = A.get(0), currVal = A.get(0);
-    		
-   		if (A.size() <= 1) {
-   			return true;
-   		}	
-   		while (currPos < A.size()-1) {    			
-   			if  (A.get(currPos + currVal) == 0) {
-    			currVal--;
-    			if (currVal == 0 ) {
-   					if (prevVal-1 == 0) {
-   						return false;
-   					}
-   					else {
-   						currPos = prevPos;
-    					currVal = prevVal -1;
-    				}	
-    			}
-    		}
-    		else if (A.get(currPos + currVal) > 0) {
-    			prevPos = currPos;
-    			prevVal = currVal;
-    			currPos = currPos + currVal;
-    			currVal = A.get(currPos);
-    		}
-    	}
-        return true;
-    }
     
 }
