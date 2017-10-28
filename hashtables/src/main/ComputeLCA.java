5ec49e9a98a0ce18d7c73a8e8383d4948f84d734
Arfan Rehab <areha308@gmail.com>
2017-10-27 19:58:48 -0700

    Compute LCA

---
+++ hashtables/src/main/ComputeLCA.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ComputeLCA {
+
+    /*
+    12.4
+    */
+
+    public static BinaryTreeParent<Integer> LCA(BinaryTreeParent<Integer> node0, BinaryTreeParent<Integer> node1) {
+    	Set<BinaryTree<Integer>> hash = new HashSet<>();
+        while (node0 != null || node1 != null) {
+          // Ascend tree in tandem from these two nodes.
+        	if (node0 != null) {
+        		if (!hash.add(node0)) {
+        			return node0;
+        		}
+        		node0 = node0.parent;
+        	}
+        	if (node1 != null) {
+        		if (!hash.add(node1)) {
+        			return node1;
+        		}
+        		node1 = node1.parent;
+        	}
+        }
+        throw new IllegalArgumentException("node0 and node1 are not in the same tree");
+    }
+}
