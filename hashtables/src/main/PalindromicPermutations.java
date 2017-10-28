e9b564ea2c7822afc3002b43f375dfa2001df6e1
Arfan Rehab <areha308@gmail.com>
2017-10-27 19:29:16 -0700

    Hash tables

---
+++ hashtables/src/main/PalindromicPermutations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PalindromicPermutations {
+
+    /*
+    12.1
+    */
+
+    public static boolean canFormPalindrome(String s) {
+    	Set<Character> charsWithOddFrequency = new HashSet<>();
+    	for (int i = 0; i < s.length; i++) {
+    		char c = s.charAt(i);
+    		if (charsWithOddFrequency.contains(c)) {
+    			charsWithOddFrequency.remove(c);
+    		}
+    		else {
+    			charsWithOddFrequency.add(c);
+    		}
+    	}
+    	return charsWithOddFrequency.size() <= 1;
+    }
+}
