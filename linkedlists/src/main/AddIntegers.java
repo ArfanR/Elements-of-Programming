0d577f01e9afba955f5a483b5e72b9c65dada6ee
Arfan Rehab <areha308@gmail.com>
2017-11-05 16:23:06 -0800

    Add Integers

---
+++ linkedlists/src/main/AddIntegers.java
@@ -0,0 +1,21 @@
+public class AddIntegers {
+
+    /*
+    7.13
+    */
+    
+    public static ListNode<Integer> addIntegers(ListNode<Integer> a, ListNode<Integer> b) {
+    	ListNode<Integer> dummyHead = new ListNode<>(0, null);
+        ListNode<Integer> placeIter = dummyHead;
+        int carry = 0;
+        while (L1 != null || L2 != null || carry != 0) {
+        	int val = carry + (L1 != null ? L1.data : 0) + (L2 != null ? L2.data : 0);
+        	L1 = L1 != null ? L1.next : null;
+        	L2 = L2 != null ? L2.next : null;
+        	placeIter.next = new ListNode<>(val % 10, null);
+        	carry = val / 10;
+        	placeIter = placeIter.next;
+        }
+        return dummyHead.next;
+    }
+}
