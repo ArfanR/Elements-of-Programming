a34049377573b61fbf7543dadbec08ddadabc135
Arfan Rehab <areha308@gmail.com>
2017-11-11 16:20:42 -0800

    Variant

--- searching/src/main/SearchSorted.java
+++ searching/src/main/SearchSorted.java
@@ -12,7 +12,7 @@ public class SearchSorted {
     		int mid = left + ( (right - left) / 2);
     		if (list.get(mid) == k) {
     			result = mid;
-    			right = mid-1;
+    			right = mid - 1;
     		}
     		else if (list.get(mid) > k) {
     			right = mid - 1;
@@ -22,6 +22,25 @@ public class SearchSorted {
     		}
     	}
     	return result;
+    }
+    
+    // Variant: find first element greater than key
+    public static int search2(List<Integer> list, int k) {
+    	int left = 0, right = list.size() - 1, result = -1;
+    	while (left <= right) {
+    		int mid = left + ( (right - left) / 2);
+    		if (list.get(mid) == k) {
+    			result = mid;
+    			left = mid + 1;
+    		}
+    		else if (list.get(mid) > k) {
+    			right = mid - 1;
+    		}
+    		else {
+    			left = mid + 1;
+    		}
+    	}
+    	return result+1 > list.size()-1 ? -1 : result+1
     }
     
     
